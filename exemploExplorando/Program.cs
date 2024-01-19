da79f23a07be5d5deeca474cc17eef772ff9cbd7
alexklenio <alexlopes.see@gmail.com>
2024-01-19 20:36:02 -0300

    arquivos
    upload dos arquivos do curso

---
+++ exemploExplorando/Program.cs
@@ -0,0 +1,13 @@
+﻿using exemploExplorando.Models;
+
+Pessoa pessoa1 = new Pessoa(nome: "Alex", sobrenome: "Lopes");
+
+Pessoa pessoa2 = new Pessoa(nome: "Caio", sobrenome: "Lopes");
+
+Curso CursoDeInglês = new Curso();
+CursoDeInglês.Nome = "Inglês";
+CursoDeInglês.Alunos = new List<Pessoa>();
+
+CursoDeInglês.AdicionarAluno(pessoa1);
+CursoDeInglês.AdicionarAluno(pessoa2);
+CursoDeInglês.ListarAlunos();
---
+++ exemploFundamentos/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using exemploFundamentos.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Alex Lopes";
+pessoa1.Idade = 37;
+
+pessoa1.Apresentar();
